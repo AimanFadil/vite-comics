194111cd9e05d4c3fc8ab134fcf427da3775736b
AimanFadil <aimanbello2001@gmail.com>
2024-01-08 17:38:10 +0100

    main imges fixed

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,23 +5,23 @@ export default {
     return {
       informazioni: [
         {
-          img: '../assets/img/buy-comics-digital-comics.png',
+          img: './public/imgs/buy-comics-digital-comics.png',
           text: 'DIGITAL COMICS'
         },
         {
-          img: '../assets/img/buy-comics-merchandise.png',
+          img: './public/imgs/buy-comics-merchandise.png',
           text: 'DC MERCHANDISE'
         },
         {
-          img: '../assets/img/buy-comics-shop-locator.png',
+          img: './public/imgs/buy-comics-shop-locator.png',
           text: 'SUBSCRIPTION'
         },
         {
-          img: '../assets/img/buy-comics-subscriptions.png',
+          img: './public/imgs/buy-comics-subscriptions.png',
           text: 'COMIC SHOP LOCATOR'
         },
         {
-          img: '../assets/img/buy-dc-power-visa.png',
+          img: './public/imgs/buy-dc-power-visa.svg',
           text: 'DC POWER VISA'
         },
       ]
@@ -39,9 +39,6 @@ export default {
         <li v-for="infos, index in informazioni" :key="index" ><img :src="infos.img" > <span>{{ infos.text}}</span></li>
       </ul>
     </div>
-    
-    
-    
   </main>
 </template>
 
@@ -52,7 +49,7 @@ export default {
 
   .textWhite {
     color: white;
-    text-align: left;
+
   }
 }
 
@@ -62,13 +59,17 @@ export default {
   background-color: rgba(2, 130, 249, 255);
   height: 200px;
 
+  img {
+    width: 35%;
+  }
+
   .info {
     display: flex;
     flex-direction: row;
 
     li {
       list-style: none;
-      padding: 50px;
+      padding-top: 50px;
       color: white;
     }
 
